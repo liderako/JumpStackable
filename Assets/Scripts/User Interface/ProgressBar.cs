74cc16c70c892c5259904773b11f9a0168462e41
Antonio Sviridov <ant.svirido@gmail.com>
2020-12-19 20:45:54 +0200

    upload project

--- Assets/Scripts/User Interface/ProgressBar.cs
+++ Assets/Scripts/User Interface/ProgressBar.cs
@@ -1,34 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using CoreGame.Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ProgressBar : MonoBehaviour
+namespace CoreGame.UI
 {
-    [SerializeField] private Image _progressBar;
-    [SerializeField] private Transform _player;
-    [SerializeField] private Transform _startPosition;
-    [SerializeField] private Transform _endPosition;
-    
-    private float _pathLen;
-    private float _currentDistance;
-    private bool _finish;
+    public class ProgressBar : MonoBehaviour
+    {
+        [SerializeField] private Image _progressBar;
+        [SerializeField] private Transform _player;
+        [SerializeField] private Transform _startPosition;
+        [SerializeField] private Transform _endPosition;
+        [SerializeField] private TextMeshProUGUI _currentLevel;
+        [SerializeField] private TextMeshProUGUI _nextLevel;
+        
+        private float _pathLen;
+        private float _currentDistance;
+        private bool _finish;
 
-    private void Start()
-    {
-        _pathLen = Vector3.Distance(_startPosition.position, _endPosition.position);
-    }
-    
-    private void FixedUpdate()
-    {
-        if (_player.position.x > _endPosition.position.x)
+        private void Start()
         {
-            _progressBar.fillAmount = 1;
-            UIManager.Instance.FinalJump();
-            return;
+            _pathLen = Vector3.Distance(_startPosition.position, _endPosition.position);
         }
-        _currentDistance = _pathLen - Vector3.Distance(_endPosition.position, _player.position);
-        _progressBar.fillAmount = _currentDistance / _pathLen;
+        
+        private void FixedUpdate()
+        {
+            if (_player.position.x > _endPosition.position.x)
+            {
+                _progressBar.fillAmount = 1;
+                UIManager.Instance.FinalJump();
+                return;
+            }
+            _currentDistance = _pathLen - Vector3.Distance(_endPosition.position, _player.position);
+            _progressBar.fillAmount = _currentDistance / _pathLen;
+        }
     }
 }
